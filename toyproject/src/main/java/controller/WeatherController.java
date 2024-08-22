f97fab61ee01540f845dbaa8d8a1e9b1136b5c3b
heejipark23 <gbfu650@gmail.com>
2024-08-22 20:56:13 +0900

    수정

--- toyproject/src/main/java/controller/WeatherController.java
+++ toyproject/src/main/java/controller/WeatherController.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.TimeZone;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
